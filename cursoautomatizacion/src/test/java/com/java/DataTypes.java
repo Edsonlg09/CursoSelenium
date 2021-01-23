837f8cfdcf779f80b419f91e8b76b4620a539572
Edsonlg09 <glae_09@hotmail.com>
2021-01-23 12:26:20 -0600

    Inicio java

---
+++ cursoautomatizacion/src/test/java/com/java/DataTypes.java
@@ -0,0 +1,33 @@
+package com.java;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte variableByte = 18;
+		
+		int variableInt = 1234;
+		
+		System.out.println(variableInt);
+		
+		System.out.println(variableByte);
+		
+		double altura = 1.81; 
+		
+		System.out.println("Esta es la altura "+ altura);
+			
+		char letra1 = '\u0118';
+		
+		System.out.println("Este es el caracter "+ letra1);
+		
+		String frase1 = "hi";
+		String frase2 = "how are you";
+		
+		System.out.println(frase1+frase2);
+		
+		boolean isDog = true;
+
+	}
+
+}
